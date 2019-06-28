0f203a9cdd487b0cd33d99696fa46b5d0b84375a
romainquencez <romain.quencez@gmail.com>
2019-06-28 22:47:06 +0200

    add Swagger

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import (path, include)
+from rest_framework_swagger.views import get_swagger_view
 
 from . import api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', get_swagger_view(title='Docs'), name='docs'),
     path('api/', include(api_urls), name='api'),
 ]
