4e69ab30a8979b3701b19fb4fb9e70c75dac1e17
romainquencez <romain.quencez@gmail.com>
2019-06-28 22:46:13 +0200

    add API urls

---
+++ todo/api_urls.py
@@ -0,0 +1,18 @@
+from django.urls import (path, include)
+from rest_framework_jwt.views import (
+    obtain_jwt_token, refresh_jwt_token, verify_jwt_token)
+
+from tasks.urls import tasks_urlspatterns
+
+urlpatterns = [
+    # DRF api auth
+    path(
+        'api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+    # JWT AUTH
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
+    # apps
+    path('tasks/', include(tasks_urlspatterns)),
+]
