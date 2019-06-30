a3c19400ee67b3afca778fc0e86640e37f42cc7b
romainquencez <romain.quencez@gmail.com>
2019-06-30 10:41:36 +0200

    add mutations

---
+++ frontend/src/store/mutation-types.js
@@ -0,0 +1,13 @@
+// Auth
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
+export const SET_TOKEN = 'SET_TOKEN'
+
+// User
+export const SET_USER_INFO = 'SET_USER_INFO'
+
+// Tasks
+export const SET_TASKS = 'SET_TASKS'
+export const CREATE_TASK = 'CREATE_TASK'
+export const UPDATE_TASK = 'UPDATE_TASK'
+export const DELETE_TASK = 'DELETE_TASK'
---
+++ frontend/src/store/mutations.js
@@ -0,0 +1,36 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.LOGIN] (state) {
+    state.authenticated = true
+  },
+  [types.LOGOUT] (state) {
+    state.authenticated = false
+    state.token = null
+  },
+  [types.SET_TOKEN] (state, { token }) {
+    state.token = token
+  },
+  [types.SET_USER_INFO] (state, { user }) {
+    state.me = user
+  },
+  [types.SET_TASKS] (state, { tasks }) {
+    state.tasks = tasks
+  },
+  [types.CREATE_TASK] (state, { task }) {
+    state.tasks.push(task)
+  },
+  [types.UPDATE_TASK] (state, { task }) {
+    state.tasks.splice(
+      state.tasks.findIndex(item => item.id === task.id),
+      1,
+      task
+    )
+  },
+  [types.DELETE_TASK] (state, { task }) {
+    state.tasks.splice(
+      state.tasks.findIndex(item => item.id === task.id),
+      1
+    )
+  }
+}
