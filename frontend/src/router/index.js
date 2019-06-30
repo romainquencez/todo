6778c015d20996b671178ff8743bea1649047374
romainquencez <romain.quencez@gmail.com>
2019-06-30 10:42:45 +0200

    add router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/connexion',
+      name: 'login',
+      component: Login
+    }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
