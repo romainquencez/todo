14a33387cd195bcb0f105b0eed26ceb9ab15e050
romainquencez <romain.quencez@gmail.com>
2019-07-04 20:35:43 +0200

    add navbar

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,24 +1,40 @@
 <template>
-  <section>
-    <nav
-      class="navbar"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="#"
-        >
-          Todo
-        </a>
-      </div>
-    </nav>
-  </section>
+  <nav
+    class="navbar is-dark"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <router-link
+        :to="{ name: 'home' }"
+        class="navbar-item is-uppercase"
+        href="#"
+      >
+        Todo
+      </router-link>
+    </div>
+
+    <div class="navbar-end">
+      <template v-if="authenticated">
+        <div class="navbar-item">
+          connecté !
+        </div>
+      </template>
+
+      <template v-else>
+        <div class="navbar-item">
+          pas connecté
+        </div>
+      </template>
+    </div>
+  </nav>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  computed: mapState([ 'authenticated', 'me' ])
 }
 </script>
