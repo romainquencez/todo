d47ac9b3c1f428ba83a4208c41a01d339150081e
romainquencez <romain.quencez@gmail.com>
2019-07-30 22:13:28 +0200

    update ports

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://backend:8200',
+        target: 'http://backend:8080',
         changeOrigin: true
       }
     },
