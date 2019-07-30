f84d3838bd4a267e3bcf5d7cea539770369c16c8
romainquencez <romain.quencez@gmail.com>
2019-07-30 21:23:39 +0200

    tasks - change issue field to PositiveIntegerField

--- tasks/models.py
+++ tasks/models.py
@@ -5,5 +5,5 @@ class Task(models.Model):
     name = models.CharField('Nom', max_length=500)
     description = models.TextField(blank=True)
     creation_date = models.DateField('Date de création', auto_now=True)
-    issue = models.PositiveSmallIntegerField('Numéro de ticket', null=True)
+    issue = models.PositiveIntegerField('Numéro de ticket', null=True)
     done = models.BooleanField('Terminé ?', default=False)
